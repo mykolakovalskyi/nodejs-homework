774056539612d8a8f6a848674c96dcf745055a50
Mykola Kovalskyi <112631611+mykolakovalskyi@users.noreply.github.com>
2023-08-12 18:41:31 +0200

    express api completed

--- models/contacts.js
+++ models/contacts.js
@@ -1,14 +1,133 @@
-// const fs = require('fs/promises')
+const fs = require("fs").promises;
+const path = require("path");
+const { nanoid } = require("nanoid");
 
-const listContacts = async () => {}
+const contactsPath = path.resolve("./models/contacts.json");
 
-const getContactById = async (contactId) => {}
+function listContacts() {
+  const contacts = fs
+    .readFile(contactsPath)
+    .then((data) => {
+      const contacts = JSON.parse(data);
+      console.log("List of contacts: ");
+      console.table(contacts);
+      return contacts;
+    })
+    .catch((err) => console.log(err.message));
+  return contacts;
+}
 
-const removeContact = async (contactId) => {}
+function getContactById(contactId) {
+  const contact = fs
+    .readFile(contactsPath)
+    .then((data) => {
+      const contacts = JSON.parse(data);
+      const contact = contacts.find((contact) => {
+        if (contact.id === contactId) {
+          console.log(`Get contact by ID ${contactId}:`);
+          console.table(contact);
+          return contact;
+        }
+      });
 
-const addContact = async (body) => {}
+      if (contact == null) {
+        console.log(`Contact with ID "${contactId}" not found!`);
+        return;
+      }
 
-const updateContact = async (contactId, body) => {}
+      return contact;
+    })
+    .catch((err) => console.log(err.message));
+  return contact;
+}
+
+function removeContact(contactId) {
+  const newContacts = fs
+    .readFile(contactsPath)
+    .then((data) => {
+      const contacts = JSON.parse(data);
+      const newContacts = contacts.filter(
+        (contact) => contact.id !== contactId
+      );
+
+      if (newContacts.length === contacts.length) {
+        console.log(`Contact with ID "${contactId}" not found!`);
+        return contacts;
+      }
+
+      console.log("Contact deleted successfully! New list of contacts: ");
+      console.table(newContacts);
+
+      fs.writeFile(contactsPath, JSON.stringify(newContacts), (error) => {
+        if (error) {
+          return console.log("error :", error);
+        }
+      });
+      return newContacts;
+    })
+    .catch((err) => console.log(err.message));
+  return newContacts;
+}
+
+function addContact(name, email, phone) {
+  const newContact = fs
+    .readFile(contactsPath)
+    .then((data) => {
+      const contacts = JSON.parse(data);
+
+      const contact = {
+        id: nanoid(),
+        name: name,
+        email: email,
+        phone: phone,
+      };
+
+      contacts.push(contact);
+
+      console.log("Contacts added successfully! New lists of contacts: ");
+      console.table(contacts);
+
+      fs.writeFile(contactsPath, JSON.stringify(contacts), (error) => {
+        if (error) {
+          return console.log(error);
+        }
+      });
+
+      return contact;
+    })
+    .catch((err) => console.log(err.message));
+  return newContact;
+}
+
+function updateContact(contactId, name, email, phone) {
+  const updatedContact = fs.readFile(contactsPath).then((data) => {
+    const contacts = JSON.parse(data);
+    const contact = contacts.find((contact) => {
+      if (contact.id === contactId) {
+        contact.name = name ? name : contact.name;
+        contact.email = email ? email : contact.email;
+        contact.phone = phone ? phone : contact.phone;
+        console.log(`Contact with ID ${contactId} updated!`);
+        console.table(contacts);
+        return contact;
+      }
+    });
+
+    if (contact == null) {
+      console.log(`Contact with ID "${contactId}" not found!`);
+      return;
+    }
+
+    fs.writeFile(contactsPath, JSON.stringify(contacts), (error) => {
+      if (error) {
+        return console.log(error);
+      }
+    });
+
+    return contact;
+  });
+  return updatedContact;
+}
 
 module.exports = {
   listContacts,
@@ -16,4 +135,4 @@ module.exports = {
   removeContact,
   addContact,
   updateContact,
-}
+};
